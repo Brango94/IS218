0726456e8145c73d7eedfb92f26800cdf96e24b5
Brango94 <35547607+Brango94@users.noreply.github.com>
2018-12-06 21:44:14 -0500

    MVC

---
+++ View/db_error.php
@@ -0,0 +1,8 @@
+<?php include '../view/header.php'; ?>
+
+    <h1>Database Error</h1>
+    There was an error connecting to the database.</p>
+    
+   	Error message: <?php echo $error_message; ?></p>
+   	
+</html>
