912595f88ce042c364bb09653bd074c3078dd1f8
Brango94 <35547607+Brango94@users.noreply.github.com>
2018-12-21 19:55:13 -0500

    P4

--- Model/database.php
+++ Model/database.php
@@ -1,16 +1,28 @@
 <?php
 
-	$servername= "sql1.njit.edu";
-	$username="bsg23";
-	$project="bsg23";
-	$password= "Faly0aJZJ";
-	
-	try{
-		$conn = new PDO("mysql:host=$servername;dbname=bsg23", $username, $password);
-	}
-	catch(PDOException $e){
-		$error_message= $e->getMessage();
-		include('db_error.php');
-		exit();
-	}
+class Database {
+
+    private static $dsn= "mysql:host=sql1.njit.edu;dbname=bsg23";
+    private static $username = "bsg23";
+    private static $project="bsg23";
+    private static $password = "Faly0aJZJ";
+    private static $conn;
+    private function __construct() {}
+    public static function database () {
+        if (!isset(self::$conn)) {
+            try {
+                self::$conn = new PDO(self::$dsn,
+                                     self::$username,
+                                     self::$password);
+            } 
+            catch(PDOException $e){
+				$error_message= $e->getMessage();
+				include('db_error.php');
+				exit();
+            }
+        }
+        return self::$conn;
+    }
+}
+?>
 ?>
