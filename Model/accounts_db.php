83e313f8bfe2a5ab70d11f4cd4c0a9331ce57693
Brango94 <35547607+Brango94@users.noreply.github.com>
2018-12-21 19:53:26 -0500

    P4

--- Model/accounts_db.php
+++ Model/accounts_db.php
@@ -1,18 +1,107 @@
 <?php
-function get_user($email,$pass){
+
+function get_all(){
 	global $conn;
-	$stmt = $conn->prepare('SELECT * FROM accounts WHERE email=? AND password=?');
-	$stmt->bindParam(1, $email, PDO::PARAM_INT);
-	$stmt->bindParam(2, $pass, PDO::PARAM_INT);
-	$stmt->execute();
-	$row = $stmt->fetch(PDO::FETCH_ASSOC);
-	$stmt->closeCursor();
-	return $row;
+	$query = 'SELECT * FROM accounts ORDER BY id';
+	$statement = $db->prepare($query);
+	$statement->execute();
+	return $statement; 
 }
-function create_user($email,$first,$last,$bday,$pass){
-	global $conn;
-	$stmt = $conn->prepare("INSERT INTO accounts (email,fname,lname,birthday,password) 
-			VALUES ('$email','$first','$last','$bday','$pass')");
-	$stmt->execute();
-}
+
+class UsersDB 
+	{
+	    public static function get_all() 
+	    {
+	        $db = Database::database();
+	        $query = 'SELECT * FROM accounts';
+	        $statement = $db->prepare($query);
+	        $statement->execute();
+	        
+	        $accounts = array();
+	        foreach ($statement as $row)
+	        {
+	            $account = new User($row['email'],$row['fname'],$row['lname'],$row['birthday'],$row['password']);
+	            $accounts[] = $account;
+	        }
+	        return $accounts;
+	    }
+	    public static function get_user($email,$pass){
+	    	$db = Database::database();
+			$stmt = $db->prepare("SELECT * FROM accounts WHERE email='$email' AND password='$pass' ");
+			$stmt->execute();
+			$acct = array();
+			foreach ($stmt as $row)
+	        {
+	            $account = new User($row['id'],$row['email'],$row['fname'],$row['lname'],$row['birthday'],$row['password']);
+	            $acct[] = $account;
+	        }
+			return $acct;
+		
+		}
+	    public static function create_user($email,$first,$last,$bday,$pass)
+	    {
+	    	$db = Database::database();
+	    	$query = "INSERT INTO accounts (email,fname,lname,birthday,password) VALUES ('$email','$first','$last','$bday','$pass') ";
+	    	$statement = $db->prepare ($query);
+	    	$statement->execute();
+	    	$statement->closeCursor();
+		}
+	}
+	
+class User
+	{
+		private $id;
+		private $email;
+		private $fname;
+		private $lname;
+		private $bday;
+		private $pass;
+		public function __construct($Id, $Email, $Fname, $Lname, $Birthday, $Password) 
+		{
+	        $this->id = $Id;
+	        $this->email = $Email;
+	        $this->fname = $Fname;
+	        $this->lname = $Lname;
+	        $this->bday = $Birthday;
+	        $this->pass = $Password;
+    	}
+		public function getId()
+		{
+			return $this->id;
+		}
+		
+		public function getEmail()
+		{
+			return $this->email;
+		}
+		
+		public function getFname()
+		{
+			return $this->fname;
+		}
+		
+		public function getLname()
+		{
+			return $this->lname;
+		}
+				
+		public function getBirthday()
+		{
+			return $this->bday;
+		}
+		public function getPass()
+		{
+			return $this->pass;
+		}
+		public function displayUserRow() {
+			
+			return "<td>$this->id</td>
+
+				<td>$this->owneremail</td>
+				<td>$this->fname</td>
+				<td>$this->bday </td>
+				<td>$this->lname</td>";
+	
+		}
+	}
 ?>
